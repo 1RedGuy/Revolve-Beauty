f4d1a368409ff4b069f52b62822178e806f36945
1RedGuy <119105124+1RedGuy@users.noreply.github.com>
2025-02-01 18:51:27 +0200

    c

--- revolve-beauty/pages/_app.js
+++ revolve-beauty/pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/global.css";
 import { LanguageProvider } from "../context/LanguageContext";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
       <Component {...pageProps} />
+      <SpeedInsights />
     </LanguageProvider>
   );
 }
